fdd17ec93f015ff35b14b444b038600c3c164d9e
verci-eth <hello@vercingetorige.tech>
2023-01-11 22:22:01 +0100

    web-28 box shadow blog css header css

--- src/Blog.js
+++ src/Blog.js
@@ -1,20 +1,23 @@
 import React from "react";
 import telegramLogo from './utils/telegram.svg';
+import './Blog.css';
 
 function Blog() {
     const TELEGRAM_LINK = 'https://t.me/sonoliberoio';
     return(
         <div className="card">
-            <p>sonolibero è un racconto.. ✍🏽</p>
-            <p>
-                <div>di un viaggio sul Pianeta Terra, 🌏</div>
-                <div>unica nostra amata casa. 🏕️</div>
-            </p>
-            <p>
-                <div>un alternarsi di movimento e pause, 🧘🏽</div>
-                <div>in cerca di domande migliori. 💭</div>
-            </p>
-            <p>vuoi salire a bordo?</p>
+            <div className="blog">
+                <p>sonolibero è un racconto.. ✍🏽</p>
+                <p>
+                    <div>di un viaggio sul Pianeta Terra, 🌏</div>
+                    <div>unica nostra amata casa. 🏕️</div>
+                </p>
+                <p>
+                    <div>un alternarsi di movimento e pause, 🧘🏽</div>
+                    <div>in cerca di domande migliori. 💭</div>
+                </p>
+                <p>vuoi salire a bordo?</p>
+            </div>
             <a href={TELEGRAM_LINK} target="_blank" rel="noreferrer">
                 <img alt="Telegram Logo" className="icon" src={telegramLogo} />
             </a>
